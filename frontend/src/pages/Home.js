ef221ece7880bd0b018ba4fc8239d70210af8e1d
Tirta <gamingkoper667@gmail.com>
2025-04-05 15:25:46 +0700

    changes

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Home() {
+
+  const [readData, setReadData] = useState([])
+
+  return (
+    <div>
+        <h2>Home</h2>
+    </div>
+  )
+}
+
+export default Home
